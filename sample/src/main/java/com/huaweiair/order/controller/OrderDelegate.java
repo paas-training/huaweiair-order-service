5e251820f9d03b5b1f930e6e04b1c3fc99cd918d
Maoxuepeng <m00247266@CHINA.HUAWEI.COM>
2017-11-03 09:11:17 +0800

    修改参考代码策略，学员只需要将dao代码复制到源代码目录，其他的使用生成的，然后再编写部分代码

--- sample/src/main/java/com/huaweiair/order/controller/OrderDelegate.java
+++ sample/src/main/java/com/huaweiair/order/controller/OrderDelegate.java
@@ -1,100 +1,47 @@
 package com.huaweiair.order.controller;
 
-import com.huaweiair.order.dao.MemOrderDBAdapterImpl;
-import com.huaweiair.order.dao.MysqlOrderDbAdapterImpl;
-import com.huaweiair.order.dao.OrderDbAdapter;
+import com.huaweiair.order.dao.OrderDelegateImpl;
 import com.huaweiair.order.model.FlightFlag;
 import com.huaweiair.order.model.FlightOrder;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderDelegate{
-
-	private OrderDbAdapter dbAdapter;
-	private static final Logger LOGGER = LoggerFactory.getLogger(OrderDelegate.class);
+	
+	private OrderDelegateImpl delegateImpl;
 	
 	public OrderDelegate() {
-	    String ip = System.getenv("MYSQL_DB_IP");
-	    String port = System.getenv("MYSQL_DB_PORT");
-
-		if (null == ip || null == port) {
-			dbAdapter = new MemOrderDBAdapterImpl();
-		} else {
-		    LOGGER.info("mysql ip: {} port: {}",ip,port);
-		    dbAdapter =
-		        new MysqlOrderDbAdapterImpl(ip, Integer.valueOf(port));			
-		}
+		delegateImpl = new OrderDelegateImpl();
 	}
-	
-    public Boolean createOrders(FlightFlag order){
-    	if (order == null) {
-    		return false;
-    	}
-        boolean flagOne = false;
-        boolean flagTwo = false;
-        Date date = new Date();  
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-        String dateNowStr = simpleDateFormat.format(date);  
-        if (null != order.getToFlightId() && !order.getToFlightId().isEmpty()) {
-          FlightOrder toFlightOrder = new FlightOrder();
-          toFlightOrder.setUserId(order.getUserId());
-          toFlightOrder.setOrderId(UUID.randomUUID().toString());
-          toFlightOrder.setFlightId(order.getToFlightId());
-          toFlightOrder.setName(order.getFromAirPortName() + "-" + order.getToAirPortName());
-          toFlightOrder.setScheduledDepartureTime(order.getToScheduledDepartureTime());
-          toFlightOrder.setScheduledArrivalTime(order.getToScheduledArrivalTime());
-          toFlightOrder.setFlightClass(order.getToFlightClass());
-          toFlightOrder.setFlightPrice(order.getToFlightPrice());
-          toFlightOrder.setOrderTime(dateNowStr);
-          toFlightOrder.setOrderStatus(0);
-          flagOne = dbAdapter.insertOrder(toFlightOrder);
-        }
-        if (null != order.getRetFlightId() && !order.getRetFlightId().isEmpty()) {
-          FlightOrder returnFlightOrder = new FlightOrder();
-          returnFlightOrder.setUserId(order.getUserId());
-          returnFlightOrder.setOrderId(UUID.randomUUID().toString());
-          returnFlightOrder.setFlightId(order.getRetFlightId());
-          returnFlightOrder.setName(order.getToAirPortName() + "-" + order.getFromAirPortName());
-          returnFlightOrder.setScheduledDepartureTime(order.getRetScheduledDepartureTime());
-          returnFlightOrder.setScheduledArrivalTime(order.getRetScheduledArrivalTime());
-          returnFlightOrder.setFlightClass(order.getRetFlightClass());
-          returnFlightOrder.setFlightPrice(order.getRetFlightPrice());
-          returnFlightOrder.setOrderTime(dateNowStr);
-          returnFlightOrder.setOrderStatus(0);
 
-          flagTwo = dbAdapter.insertOrder(returnFlightOrder);
-        }
-        return flagOne && flagTwo;
+    public Boolean createOrders(FlightFlag order){
+
+        // Do Some Magic Here!
+        return delegateImpl.createOrders(order);
     };
 
 
     public Boolean deleteOrder(String orderId){
-    	if (orderId == null) {
-    		return false;
-    	}
-        return dbAdapter.deleteOrder(orderId);
+
+        // Do Some Magic Here!
+        return delegateImpl.deleteOrder(orderId);
     };
 
 
     public List<FlightOrder> getAllOrders(String userId){
-        return Arrays.asList(dbAdapter.queryOrders(userId));
+
+        // Do Some Magic Here!
+        return delegateImpl.getAllOrders(userId);
     };
 
 
     public Boolean modifyOrder(String orderId, Integer action){
-    	if (orderId == null || action == null) {
-    		return false;
-    	}
-        return dbAdapter.updateOrder(orderId, action);
+
+        // Do Some Magic Here!
+        return delegateImpl.modifyOrder(orderId, action);
     };
 
 }
